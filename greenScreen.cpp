a8a37468f068f87f173f45a146f0ea20a6db266c
Harshit <nowyouseeme2203@gmail.com>
2026-03-21 03:24:47 +0530

    Implemented resize

--- greenScreen.cpp
+++ greenScreen.cpp
@@ -14,6 +14,26 @@ bool greenScreenImage::checkAspectRatio(const Image &img1, const Image &img2)
 
 int greenScreenImage::checkSizes(const Image &img1, const Image &img2)
 {
+
+        if(checkAspectRatio(img1, img2) == false)
+        {
+                return -1;
+        }
+        else
+        {
+                if(img1.getHeight() == img2.getHeight())
+                {
+                        return 0; // same size
+                }
+
+                if(img1.getHeight() > img2.getHeight())
+                {
+                        return 1; // img1 is bigger
+                }
+
+                return 2; // img2 is bigger
+        }
+
 }
 
 void greenScreenImage::resizeBigToSmall(Image &big, Image &small)
